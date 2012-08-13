e34789479e72c462042353e82ecb6c45563fa764
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2012-08-13 12:05:41 -0400

    Initial Commit

---
+++ RCMP for Real Git/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RCMP for Real Git
+//
+//  Created by Andrew Wilcox on 2012-Aug-13.
+//  Copyright (c) 2012 Wilcox Technologies LLC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
